25a933692fa2826eb3399a3153c4cba1c5a17457
hirocode321 <sozosha.sekioka@gmail.com>
2026-01-28 00:09:43 +0900

    app.pyの変更

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+"""
 if __name__ == '__main__':
     # init_db()
     app.run(port=5555, debug=True)
+"""
